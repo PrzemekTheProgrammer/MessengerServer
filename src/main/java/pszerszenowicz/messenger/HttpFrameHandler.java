49caf7d0d80769bd241e7def56560881dbb6be8e
Przemek <przemyslaw.szerszenowicz@gmail.com>
2024-12-16 19:53:03 +0100

    Updated 1st ver

--- src/main/java/pszerszenowicz/messenger/HttpFrameHandler.java
+++ src/main/java/pszerszenowicz/messenger/HttpFrameHandler.java
@@ -3,13 +3,11 @@ package pszerszenowicz.messenger;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
-import io.netty.handler.codec.http.*;
-import io.netty.util.CharsetUtil;
 
 import java.util.List;
 
 
-public class HttpFrameHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
+public class HttpFrameHandler extends SimpleChannelInboundHandler<String> {
 
     private Server server;
     private Integer clientNumber;
@@ -21,12 +19,7 @@ public class HttpFrameHandler extends SimpleChannelInboundHandler<FullHttpReques
 
     @Override
     public void channelActive(ChannelHandlerContext ctx) throws Exception {
-        FullHttpResponse response = new DefaultFullHttpResponse(
-            HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
-        response.content().writeBytes(clientNumber.toString().getBytes());
-        response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
-        response.headers().set(HttpHeaderNames.CONTENT_LENGTH, response.content().readableBytes());
-        ctx.writeAndFlush(response);
+        ctx.writeAndFlush("Your client number: " + clientNumber.toString());
     }
 
     @Override
@@ -35,22 +28,16 @@ public class HttpFrameHandler extends SimpleChannelInboundHandler<FullHttpReques
     }
 
     @Override
-    protected void channelRead0(ChannelHandlerContext channelHandlerContext, FullHttpRequest fullHttpRequest) throws Exception {
-
-        if(fullHttpRequest.headers().contains("new_connection")) {
-            int receiver = Integer.parseInt(fullHttpRequest.headers().get("new_connection"));
+    protected void channelRead0(ChannelHandlerContext channelHandlerContext, String message) throws Exception {
+        if(message.startsWith("connect ")) {
+            int receiver = Integer.parseInt(message.split(" ")[2]);
             server.addConnection(clientNumber,receiver);
+            channelHandlerContext.channel().writeAndFlush("Connected!");
         }
         else {
             List<Channel> connections = server.getConnections(clientNumber);
-            String message = fullHttpRequest.content().toString(CharsetUtil.UTF_8);
-            FullHttpResponse response = new DefaultFullHttpResponse(
-                    HttpVersion.HTTP_1_1, HttpResponseStatus.OK);
-            response.content().writeBytes(message.getBytes());
-            response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain");
-            response.headers().set(HttpHeaderNames.CONTENT_LENGTH, response.content().readableBytes());
             connections.forEach((channel) -> {
-                channel.writeAndFlush(response);
+                channel.writeAndFlush(message);
             });
         }
     }
